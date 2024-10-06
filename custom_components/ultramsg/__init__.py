126bdab3112ee541d7b19b440ea847870322851a
Itay Sharoni <110981018+Itay-Sharoni@users.noreply.github.com>
2024-10-06 17:30:17 +0300

    Update __init__.py

--- custom_components/ultramsg/__init__.py
+++ custom_components/ultramsg/__init__.py
@@ -1,40 +1,15 @@
 """The UltraMsg integration."""
-import logging
-
-import voluptuous as vol
-
-from homeassistant.const import CONF_TOKEN
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import discovery
-from homeassistant.helpers.typing import ConfigType
 
-from .const import DOMAIN, CONF_INSTANCE_ID
+from .const import DOMAIN
 
-_LOGGER = logging.getLogger(__name__)
-
-CONFIG_SCHEMA = vol.Schema(
-    {
-        DOMAIN: vol.Schema(
-            {
-                vol.Required(CONF_INSTANCE_ID): str,
-                vol.Required(CONF_TOKEN): str,
-            }
-        )
-    },
-    extra=vol.ALLOW_EXTRA,
-)
-
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the UltraMsg integration."""
     hass.data.setdefault(DOMAIN, {})
-
-    if DOMAIN in config:
-        hass.data[DOMAIN] = config[DOMAIN]
-        await discovery.async_load_platform(hass, "notify", DOMAIN, {}, config)
-
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up UltraMsg from a config entry."""
     hass.data[DOMAIN][entry.entry_id] = entry.data
     hass.async_create_task(
@@ -42,7 +17,7 @@ async def async_setup_entry(hass: HomeAssistant, entry):
     )
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload an UltraMsg config entry."""
     await hass.config_entries.async_forward_entry_unload(entry, "notify")
     hass.data[DOMAIN].pop(entry.entry_id)
