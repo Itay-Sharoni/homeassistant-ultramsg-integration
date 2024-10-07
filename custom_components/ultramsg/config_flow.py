14dc4be707acdfe470bb835516d40486980b467f
Itay Sharoni <110981018+Itay-Sharoni@users.noreply.github.com>
2024-10-07 21:29:53 +0300

    Update config_flow.py

--- custom_components/ultramsg/config_flow.py
+++ custom_components/ultramsg/config_flow.py
@@ -1,5 +1,3 @@
-# custom_components/ultramsg/config_flow.py
-
 from homeassistant import config_entries
 from homeassistant.core import callback
 from .const import DOMAIN
@@ -11,8 +9,13 @@ class UltraMSGConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step of the config flow."""
-        return self.async_abort(reason="ui_not_required")
-
+        # Attempt to check if UltraMSG is already configured via configuration.yaml
+        notify_services = self.hass.services.async_services().get("notify", {})
+        if "ultramsg" in notify_services:
+            return self.async_abort(reason="UltraMSG is already configured via `configuration.yaml")
+        else:
+            return self.async_abort(reason="Config is Missing, UltraMSG is configured via `configuration.yaml`.")
+    
     @callback
     def async_get_options_flow(self):
         """No options flow."""
