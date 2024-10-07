74a35318e995b50949491949ed3320373158cd26
Itay Sharoni <110981018+Itay-Sharoni@users.noreply.github.com>
2024-10-07 21:09:22 +0300

    Update config_flow.py

--- custom_components/ultramsg/config_flow.py
+++ custom_components/ultramsg/config_flow.py
@@ -1,14 +1,17 @@
+# custom_components/ultramsg/config_flow.py
+
 from homeassistant import config_entries
 from homeassistant.core import callback
+from .const import DOMAIN
 
-class UltraMSGConfigFlow(config_entries.ConfigFlow, domain='ultramsg'):
-    """Handle a config flow for UltraMSG."""
+class UltraMSGConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle the config flow for UltraMSG."""
 
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
-        return self.async_abort(reason="not_needed")
+        """Handle the initial step of the config flow."""
+        return self.async_abort(reason="ui_not_required")
 
     @callback
     def async_get_options_flow(self):
