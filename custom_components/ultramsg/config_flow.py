b5eb240be3863a0a8683478e173310aea4088e40
Itay Sharoni <110981018+Itay-Sharoni@users.noreply.github.com>
2024-10-07 21:59:54 +0300

    Update config_flow.py

--- custom_components/ultramsg/config_flow.py
+++ custom_components/ultramsg/config_flow.py
@@ -10,6 +10,7 @@ class UltraMSGConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step of the config flow."""
+        
         if self.hass.data.get(DOMAIN, {}).get('configured'):
             message = "UltraMSG is already configured via configuration.yaml. No further action is needed."
         else:
@@ -24,7 +25,7 @@ class UltraMSGConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         )
 
         # Abort the flow without a reason (since we can't display messages directly)
-        return self.async_abort(reason="")
+        return self.async_abort(reason="message")
 
     @callback
     def async_get_options_flow(self):
